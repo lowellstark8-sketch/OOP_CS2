051e6c422787b526a69426259c3088f0cd123efc
lowellstark8-sketch <lowellstark8@gmail.com>
2026-02-20 08:14:23 +0800

    first commit

---
+++ Assignment 1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void swapVal (int *a, int *b){
+    int swap = *a;
+    *a = *b;
+    *b = swap;
+}
+
+int main() {
+    int n1, n2;
+    
+    cout << "x = ";
+    cin >> n1;
+    
+    cout << "y = ";
+    cin >> n2;
+    cout << endl; 
+
+    cout << "Before Swapping:\n";
+    cout << "x = " << n1 << endl;
+    cout << "y = " << n2 << endl;
+
+    int *a = &n1;
+    int *b = &n2;
+    
+    swapVal(&n1, &n2);
+
+    cout << "\nAfter Swapping:\n";
+    cout << "x = " << n1 << endl;
+    cout << "y = " << n2 << endl;
+
+    return 0;
+}
